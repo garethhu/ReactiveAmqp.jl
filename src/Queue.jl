7c1dbbf8dd8ad358596f0fa4d9ac9a52629adffa
garethhu <garethjhumphries@gmail.com>
2021-02-18 19:26:51 +0000

    updated queue methods

--- src/Queue.jl
+++ src/Queue.jl
@@ -8,3 +8,13 @@ function declare_queue!(chan, queue)
     success, queue_name, message_count, consumer_count = queue_declare(chan, queue)
     success || throw(QueueDeclarationError(queue_name))
 end
+
+serialize(serializable::String) = Vector{UInt8}(serializable)
+
+compose(message::T where T) = Message(serialize(message), content_type="text/plain", delivery_mode=PERSISTENT)
+
+send!(exchange::String, message::T where T, routing_key::String) =
+    basic_publish(chan, compose(message); exchange=exchange, routing_key=routing_key)
+
+send!(exchange::String, message::T where T) =
+    send!(exchange, message, "*")
