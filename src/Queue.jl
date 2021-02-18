126e85e43108153e0e09685643c36b46b417bcd3
garethhu <garethjhumphries@gmail.com>
2021-02-18 17:36:57 +0000

    added ! to declare_queue

--- src/Queue.jl
+++ src/Queue.jl
@@ -4,7 +4,7 @@ struct QueueDeclarationError <: Exception
     queue_name::String
 end
 
-function declare_queue(chan, queue)
+function declare_queue!(chan, queue)
     success, queue_name, message_count, consumer_count = queue_declare(chan, queue)
     success || throw(QueueDeclarationError(queue_name))
 end
