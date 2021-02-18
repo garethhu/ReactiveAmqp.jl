5290154bc267ac469d9ce1beb0f60d60db85e1dc
garethhu <garethjhumphries@gmail.com>
2021-02-18 20:10:11 +0000

    correct use of ! to indicate side effects

--- src/Channel.jl
+++ src/Channel.jl
@@ -1,21 +1,21 @@
 using AMQPClient
 
-AMQPClient.channel(conn) =
+channel!(conn) =
     channel(conn, AMQPClient.UNUSED_CHANNEL, true)
 
 
-function close_channel(chan)
+function close_channel!(chan)
     if isopen(chan)
         close(chan)
         AMQPClient.wait_for_state(chan, AMQPClient.CONN_STATE_CLOSED)
     end
 end
 
-function amqp_channel(f::Function, conn)
+function amqp_channel!(f::Function, conn)
     chan = channel(conn)
     try
         f(chan)
     finally
-        close_channel(chan)
+        close_channel!(chan)
     end
 end
--- src/Connection.jl
+++ src/Connection.jl
@@ -13,22 +13,22 @@ struct AmqpConnectionDef
     AmqpConnectionDef(host,auth_params) = new("/",host,AMQPClient.AMQP_DEFAULT_PORT,auth_params,Nothing())
 end
 
-AMQPClient.connection(conn_def::AmqpConnectionDef) =
+connection!(conn_def::AmqpConnectionDef) =
     connection(; virtualhost=conn_def.virtualhost, host=conn_def.host, port=conn_def.port, auth_params=conn_def.auth_params, amqps=conn_def.amqps)
 
 
-function close_connection(conn)
+function close_connection!(conn)
     if isopen(conn)
         close(conn)
         AMQPClient.wait_for_state(conn, AMQPClient.CONN_STATE_CLOSED)
     end
 end
 
-function amqp_connection(f::Function, conn_def::AmqpConnectionDef)
+function amqp_connection!(f::Function, conn_def::AmqpConnectionDef)
     conn = connection(conn_def)
     try
         f(conn)
     finally
-        close_connection(conn)
+        close_connection!(conn)
     end
 end
