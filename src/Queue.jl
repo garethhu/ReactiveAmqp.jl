cf1a7f19ca16df9bf6944a6ae778a1896ea8a6e7
garethhu <garethjhumphries@gmail.com>
2021-02-19 00:09:44 +0000

    added missing args

--- src/Queue.jl
+++ src/Queue.jl
@@ -14,8 +14,8 @@ serialize(serializable::String) = Vector{UInt8}(serializable)
 
 compose(message::T where T) = Message(serialize(message), content_type="text/plain", delivery_mode=PERSISTENT)
 
-send!(exchange::String, message::T where T, routing_key::String) =
+send!(chan,exchange::String, message::T where T, routing_key::String) =
     basic_publish(chan, compose(message); exchange=exchange, routing_key=routing_key)
 
-send!(exchange::String, message::T where T) =
-    send!(exchange, message, "*")
+send!(chan,exchange::String, message::T where T) =
+    send!(chan,exchange, message, "*")
