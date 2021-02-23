51a6a4f355cc8cfa784cf7477ac35696c532fb03
garethhu <garethjhumphries@gmail.com>
2021-02-23 18:57:02 +0000

    deb

--- src/Queue.jl
+++ src/Queue.jl
@@ -53,7 +53,7 @@ function execute_queues!()
         amqp_connection!(_conn_def) do conn
             amqp_channel!(conn) do chan
                 @sync for queue in _queues
-                    @async Threads.@spawn $queue(chan)
+                    @async Threads.@spawn queue($chan)
                 end
             end
         end
