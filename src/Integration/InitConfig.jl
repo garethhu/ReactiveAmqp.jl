e6a07f43af42c3c44b3694cc6b0fe67473c86de6
garethhu <garethjhumphries@gmail.com>
2021-02-27 16:40:00 +0000

    Work on directory structure for integration

---
+++ src/Integration/InitConfig.jl
@@ -0,0 +1,10 @@
+module InitConfig
+
+include("constants.jl")
+
+function initConfig()
+    project_root = dirname(dirname(@__FILE__))
+    reactive_amqp_dir =
+end
+
+end
---
+++ src/Integration/constants.jl
@@ -0,0 +1,5 @@
+const NEW_APP_PATH = joinpath("files", "new_app")
+
+const AMQP_PATH = "amqp"
+
+const AMQP_CONNECTION_FILE = "connection.yaml"
