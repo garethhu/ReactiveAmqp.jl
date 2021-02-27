9818581a2fcb30d7d6a53851a764b2ff6a1d0c96
garethhu <garethjhumphries@gmail.com>
2021-02-27 17:34:05 +0000

    export init_config

--- src/ReactiveAmqp.jl
+++ src/ReactiveAmqp.jl
@@ -1,12 +1,10 @@
 module ReactiveAmqp
 
-export dir, root
-
 include("includes.jl")
 
 export AmqpConnectionDef, compose_auth, amqp_conn_define!
 export source!, queue!, execute_queues!
 export map_ack, safe_map_ack!, sink!
-export init_config!
+export Integration.init_config!
 
 end
