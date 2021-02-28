a1ea4169707802ced69d07394599e548033c3045
garethhu <garethjhumphries@gmail.com>
2021-02-28 00:40:56 +0000

    corrected working directory copy

--- src/Integration/InitFiles.jl
+++ src/Integration/InitFiles.jl
@@ -1,7 +1,7 @@
 include("constants.jl")
 
 function init_config!()
-    project_root = dirname(@__DIR__)
+    project_root = pwd()
     from_location = joinpath(PKG_ROOT, NEW_APP_PATH)
     template_struct = joinpath(from_location,"*")
     println(project_root)
