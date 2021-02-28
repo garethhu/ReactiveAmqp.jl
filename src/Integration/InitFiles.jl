f885087718fb4643e5efabfd12d7067082994a59
garethhu <garethjhumphries@gmail.com>
2021-02-28 00:38:09 +0000

    t

--- src/Integration/InitFiles.jl
+++ src/Integration/InitFiles.jl
@@ -4,5 +4,7 @@ function init_config!()
     project_root = dirname(@__DIR__)
     from_location = joinpath(PKG_ROOT, NEW_APP_PATH)
     template_struct = joinpath(from_location,"*")
+    println(project_root)
+    println(from_location)
     cp(template_struct, project_root)
 end
